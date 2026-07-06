2ea63aad55bfe9de937954f65cedf6df7bf3c09b
darshanbedi321-code <darshanbedi321@gmail.com>
2026-07-06 20:22:29 +0530

    dar

--- html_viewer.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-code= """"""
-components.html(code, height=600, scrolling=True)
--- pages/home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.title("this is home page")
-col1,col2,col3 = st.columns(3)
-with col1:
-    col1= st.text_input("enter your name")
-with col2:
-    col2= st.text_input("enter your name")
-with col3:
-    col3= st.text_input("enter your name")
